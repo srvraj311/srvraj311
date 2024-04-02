85c92014158651363ffa2aedd98b1ce9d44368a9
Sourabh <sourabhraj311@gmail.com>
2024-04-02 06:26:53 +0530

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,9 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
+      <Head>
         <meta name="viewport" content="width=1366 initial-scale=0.1"></meta>
-      </head>
+      </Head>
       <body className={`${inter.className}`}>{children}</body>
     </html>
   );
